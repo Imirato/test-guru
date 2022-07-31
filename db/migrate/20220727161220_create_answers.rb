1d1bd450d970635271a3b49a514fceb381b0f549
Imirato <annovikova18@yandex.ru>
2022-07-31 13:57:03 +0300

    fix: add foreign key restriction

--- db/migrate/20220727161220_create_answers.rb
+++ db/migrate/20220727161220_create_answers.rb
@@ -5,7 +5,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.text :body, null: false
       t.boolean :correct, null: false, default: false
-      t.belongs_to :question
+      t.belongs_to :question, foreign_key: true
 
       t.timestamps
     end

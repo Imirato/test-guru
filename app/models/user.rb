0e402f49b62f242dc37bd949516d5091594359b6
Imirato <annovikova18@yandex.ru>
2022-07-31 19:38:17 +0300

    feat: create instance method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  def test_list_by_level(test_level)
+    Test.where(level: test_level)
+        .joins('INNER JOIN users_tests ON tests.id = users_tests.test_id')
+        .where(users_tests: { user_id: id })
+  end
 end

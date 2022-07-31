b3dc2da23d89fcaf71cfe69738c6f54f9ad6b597
Imirato <annovikova18@yandex.ru>
2022-07-31 19:35:39 +0300

    feat: create model UserTest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_161344) do
+ActiveRecord::Schema.define(version: 2022_07_31_153219) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -52,4 +52,16 @@ ActiveRecord::Schema.define(version: 2022_07_27_161344) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users_tests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_users_tests_on_test_id"
+    t.index ["user_id"], name: "index_users_tests_on_user_id"
+  end
+
+  add_foreign_key "users_tests", "tests"
+  add_foreign_key "users_tests", "users"
 end

1cbb0e09c46ce06d37e8c7895724941c960d0f0b
Imirato <annovikova18@yandex.ru>
2022-07-31 19:38:36 +0300

    feat: create class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  def self.test_array_by_category(name)
+    Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
+        .where(categories: { title: name }).order(title: :desc)
+        .pluck(:title)
+  end
 end

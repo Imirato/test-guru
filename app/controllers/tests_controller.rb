99eb0ee05e1132d5cb7ca330527a81009b59b545
Imirato <annovikova18@yandex.ru>
2022-08-26 12:08:26 +0300

    fix

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -10,7 +10,7 @@ class TestsController < ApplicationController
   def show; end
 
   def start
-    @current_user.tests.push(@test)
+    current_user.tests.push(@test)
     redirect_to @current_user.test_passage(@test)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@
 
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-  get 'users/new'
-
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
   delete :logout, to: 'sessions#destroy'

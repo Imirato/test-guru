6968942bea62a9e56fe4d7c663bb99b68c466bc1
Imirato <annovikova18@yandex.ru>
2022-10-06 19:15:41 +0300

    fix: fix bugs

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,10 +2,10 @@
 
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :test_passages
   has_many :users, through: :test_passages
-  belongs_to :author, class_name: :User, foreign_key: :author_id
+  belongs_to :author, class_name: :Admin, foreign_key: :author_id
 
   validates :title, presence: true, uniqueness: { case_sensitive: false, scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
--- app/models/tests_user.rb
+++
@@ -1,6 +0,0 @@
-class TestsUser < ApplicationRecord
-  belongs_to :user
-  belongs_to :test
-
-  enum status: { in_progress: 0, completed: 1}
-end

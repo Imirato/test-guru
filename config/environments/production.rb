8754ac0e6540da57674101fb5d3e9894e5ceb1a9
Imirato <annovikova18@yandex.ru>
2022-11-04 18:29:42 +0300

    feat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host:'testguru11.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:'smtp.gmail.com',
+    port:587,
+    user_name:'annovikava18@gmail.com',
+    password:'brtulycisdpgarms',
+    authentication: :plain,
+    enable_starttls_auth: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

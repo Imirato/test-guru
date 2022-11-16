31c11f742c3b64d2311dc80b4eaf1d296e38d049
Imirato <annovikova18@yandex.ru>
2022-11-16 19:04:32 +0300

    fix

--- app/services/badge_issuing_service.rb
+++ app/services/badge_issuing_service.rb
@@ -15,27 +15,20 @@ class BadgeIssuingService
   private
 
   def first_successful_attempt(badge)
-    if @user.test_passages.where(test: @test).count == 1 && @test_passage.successful_passage? &&
-      @test.title == badge.object_title
-      @user.badges << badge
-    end
+    return unless @test.title == badge.object_title
+
+    give_badge(badge) if @user.test_passages.where(test: @test).count == 1 && @test_passage.successful_passage?
   end
 
   def all_tests_by_category(badge)
-    if badge.object_title == @category.title
-      all_successful_test_passages = 0
-      @category.tests.each do |test|
-        user_test_passages = test.test_passages.where(user_id: @user)
-        user_test_passages.each do |test_passage|
-          if test_passage.successful_passage?
-            all_successful_test_passages += 1
-            break
-          end
-        end
-      end
-    end
-    if all_successful_test_passages == @category.tests.count
-      @user.badges << badge
-    end
+    return unless badge.object_title == @category.title
+
+    all_successful_test_passages = TestPassage.where(user: @user, test: @category.tests)
+                                              .find_all(&:successful_passage?).map(&:test).uniq
+    give_badge(badge) if all_successful_test_passages == @category.tests.count
+  end
+
+  def give_badge(badge)
+    @user.badges << badge
   end
 end
